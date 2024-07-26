750e596d0d235e8058637907274f42cf4a45f86b
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-26 15:42:22 -0300

    adicionando entidade ContaDigital

---
+++ src/main/java/com/BankingAPI/models/ContaDigital.java
@@ -0,0 +1,33 @@
+package com.BankingAPI.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "contasDigitais")
+public class ContaDigital implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "numero", nullable = false)
+    private int numero;
+
+    @Column(name = "saldo")
+    private BigDecimal saldo;
+
+    @Column(name = "senha", nullable = false, length = 200)
+    private String senha;
+    
+}
