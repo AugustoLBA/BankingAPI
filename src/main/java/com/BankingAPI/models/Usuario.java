fa68144a56914f39b18b325c97b54423ef773864
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-26 14:51:54 -0300

    adiconando a entidade Usuario

---
+++ src/main/java/com/BankingAPI/models/Usuario.java
@@ -0,0 +1,33 @@
+package com.BankingAPI.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "nome", nullable = false, unique = true, length = 100)
+    private String username;
+    @Column(name = "senha", nullable = false, unique = false, length = 200)
+    private String senha;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false, length = 25)
+    private Role role;
+
+
+    public enum Role{
+        ROLE_ADMIN,ROLE_CLIENTE;
+    }
+}
