74fbce9a23bf27c572bf57940f44a369ef8f3510
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-26 15:25:19 -0300

    adicionando a entidade Operacao

---
+++ src/main/java/com/BankingAPI/models/Operacao.java
@@ -0,0 +1,35 @@
+package com.BankingAPI.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "operacoes")
+public class Operacao implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "dataRealizada")
+    private LocalDateTime dataRealizada;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false, length = 13)
+    private TipoOperacao tipo;
+    @Column(name = "valor", nullable = false)
+    private BigDecimal valor;
+
+
+    public enum TipoOperacao{
+        DEPOSITO,SAQUE,TRANSFERENCIA,PAGAMENTO;
+    }
+}
