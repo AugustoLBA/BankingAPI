9f1e67ef9d5c20629374317ef95255bbfffe3aa6
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-26 15:31:25 -0300

    adicionando a entidade Analista

---
+++ src/main/java/com/BankingAPI/models/Analista.java
@@ -0,0 +1,31 @@
+package com.BankingAPI.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "analistas")
+public class Analista implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "cpf", nullable = false, unique = true, length = 11)
+    private String cpf;
+
+    @Column(name = "telefone", nullable = false, length = 11)
+    private String telefone;
+}
