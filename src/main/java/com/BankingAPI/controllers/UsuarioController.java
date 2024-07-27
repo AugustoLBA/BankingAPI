4c3d42fad546d8035575160cf613f646bf2b18d5
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-27 15:20:15 -0300

    adiconando o metodo save na classe UsuarioController

--- src/main/java/com/BankingAPI/controllers/UsuarioController.java
+++ src/main/java/com/BankingAPI/controllers/UsuarioController.java
@@ -1,13 +1,26 @@
 package com.BankingAPI.controllers;
 
+import com.BankingAPI.dto.UsuarioCreateDTO;
+import com.BankingAPI.dto.UsuarioResponseDTO;
+import com.BankingAPI.models.Usuario;
 import com.BankingAPI.service.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 @RequiredArgsConstructor // Injecção de depêndencia via lombok
 @RestController
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDTO> save(@Valid  @RequestBody UsuarioCreateDTO createDTO){
+        Usuario user = usuarioService.salvar(toUsuario(createDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.toDto(user));
+
+    }
 }
