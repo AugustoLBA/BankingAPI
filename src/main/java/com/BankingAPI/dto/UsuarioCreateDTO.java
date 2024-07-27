9b5e575aaaf351daf220c8772a2b9d796199ff0d
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-27 11:09:44 -0300

    adicionando a classe UsuarioCreateDTO

---
+++ src/main/java/com/BankingAPI/dto/UsuarioCreateDTO.java
@@ -0,0 +1,24 @@
+package com.BankingAPI.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioCreateDTO {
+
+    @NotBlank
+    @Email(message = "Formato do e-mail invalido !", regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String senha;
+}
