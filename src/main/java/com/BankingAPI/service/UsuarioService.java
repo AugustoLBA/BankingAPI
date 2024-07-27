13bdcdeb29b88cd982a4d3755675ee5d7f49f38e
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-27 15:06:17 -0300

    adiconando o metodo salvar na classe UsuarioService

--- src/main/java/com/BankingAPI/service/UsuarioService.java
+++ src/main/java/com/BankingAPI/service/UsuarioService.java
@@ -1,11 +1,21 @@
 package com.BankingAPI.service;
 
+import com.BankingAPI.models.Usuario;
 import com.BankingAPI.repositories.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 @RequiredArgsConstructor // Injeção de depêndencia via lombok
 @Service
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+       return usuarioRepository.save(usuario);
+    }
+
+
 }
