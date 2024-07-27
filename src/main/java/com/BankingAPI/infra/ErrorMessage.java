f48ca0146114dff7e2ce63287e8b7cf3f9c8a9b4
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-27 15:12:38 -0300

    adicionando a classe ErrorMessage

---
+++ src/main/java/com/BankingAPI/infra/ErrorMessage.java
@@ -0,0 +1,15 @@
+package com.BankingAPI.infra;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private HttpStatus status;
+
+    private String message;
+}
