a30191f1604edd44e01ed90c532741b63afcf3c2
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-28 16:31:46 -0300

    adicionando o metodo finById na classe ContaDigitalController

--- src/main/java/com/BankingAPI/controllers/ContaDigitalController.java
+++ src/main/java/com/BankingAPI/controllers/ContaDigitalController.java
@@ -8,10 +8,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/contasDigitais")
@@ -23,5 +21,11 @@ public class ContaDigitalController {
     public ResponseEntity<ContaDigitalResponseDTO> save(@Valid @RequestBody ContaDigitalCreateDTO createDTO){
         ContaDigital contaDigital = contaDigitalService.salvar(contaDigitalService.toContaDigital(createDTO));
         return ResponseEntity.status(HttpStatus.CREATED).body(contaDigitalService.toDto(contaDigital));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ContaDigitalResponseDTO> findById(@PathVariable Long id){
+        ContaDigital contaDigital = contaDigitalService.buscarPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(contaDigitalService.toDto(contaDigital));
     }
 }
