ce5e423454dd7b27a6c0fef8fd3a98829dcf10c6
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-28 13:13:18 -0300

    ajeitando o nome do atributo na classe Cliente

--- src/main/java/com/BankingAPI/controllers/ClienteController.java
+++ src/main/java/com/BankingAPI/controllers/ClienteController.java
@@ -1,13 +1,29 @@
 package com.BankingAPI.controllers;
 
+import com.BankingAPI.dto.ClienteCreateDTO;
+import com.BankingAPI.dto.ClienteResponseDTO;
+import com.BankingAPI.models.Cliente;
 import com.BankingAPI.service.ClienteService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 @RequiredArgsConstructor // Injeção de depêndencia via lombok
 @RestController
 @RequestMapping("api/v1/clientes")
 public class ClienteController {
 
     private final ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<ClienteResponseDTO> save(@Valid @RequestBody ClienteCreateDTO createDTO){
+        Cliente cliente = clienteService.salvar(clienteService.toCliente(createDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteService.toDto(cliente));
+    }
 }
--- src/main/java/com/BankingAPI/models/Cliente.java
+++ src/main/java/com/BankingAPI/models/Cliente.java
@@ -22,12 +22,15 @@ public class Cliente implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+    
     @Column(name = "nome", nullable = false, length = 100)
     private String nome;
+
     @Column(name = "cpf", nullable = false, unique = true, length = 11)
     private String cpf;
+
     @Column(name = "telefone", nullable = false, unique = true, length = 11)
-    private String telfone;
+    private String telefone;
 
     @OneToMany(mappedBy = "cliente")
     private Set<ContaDigital> contasDigitais = new HashSet<>();
