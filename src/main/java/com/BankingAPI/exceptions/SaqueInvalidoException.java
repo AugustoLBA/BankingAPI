debd97758b5f71b7eb57d03b9ebef4f9f9694316
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-28 17:18:19 -0300

    adicionando a classe SaqueInvalidoException

---
+++ src/main/java/com/BankingAPI/exceptions/SaqueInvalidoException.java
@@ -0,0 +1,8 @@
+package com.BankingAPI.exceptions;
+
+public class SaqueInvalidoException extends RuntimeException{
+
+    public  SaqueInvalidoException(String message){
+        super(message);
+    }
+}
