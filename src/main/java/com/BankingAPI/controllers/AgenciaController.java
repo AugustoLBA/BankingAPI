8e2492506bca1e5bc6d88a97f452cfa3c21d068e
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-28 00:03:54 -0300

    adiconando o metodo save na classe AgenciaController

--- src/main/java/com/BankingAPI/controllers/AgenciaController.java
+++ src/main/java/com/BankingAPI/controllers/AgenciaController.java
@@ -1,7 +1,15 @@
 package com.BankingAPI.controllers;
 
+import com.BankingAPI.dto.AgenciaCreateDTO;
+import com.BankingAPI.dto.AgenciaResponseDTO;
+import com.BankingAPI.models.Agencia;
 import com.BankingAPI.service.AgenciaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor // Injeção de depêndencia via lombok
@@ -10,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AgenciaController {
 
     private final AgenciaService agenciaService;
+
+    @PostMapping
+    public ResponseEntity<AgenciaResponseDTO> save(@Valid @RequestBody AgenciaCreateDTO createDTO){
+        Agencia agencia = agenciaService.salvar(agenciaService.toAgencia);
+        return ResponseEntity.status(HttpStatus.CREATED).body(agenciaService.toDto(agencia));
+    }
 }
