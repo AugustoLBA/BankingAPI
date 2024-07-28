fc1216518e795916a6f42ffd4bc7272c04a8ef2e
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-28 13:06:04 -0300

    adicionando os metodos toCliente, toDto e toListDto na classe ClienteService

--- src/main/java/com/BankingAPI/service/ClienteService.java
+++ src/main/java/com/BankingAPI/service/ClienteService.java
@@ -1,11 +1,40 @@
 package com.BankingAPI.service;
 
+import com.BankingAPI.dto.ClienteCreateDTO;
+import com.BankingAPI.dto.ClienteResponseDTO;
+import com.BankingAPI.models.Cliente;
 import com.BankingAPI.repositories.ClienteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor // Injeção de depêndencia via lombok
 @Service
 public class ClienteService {
 
     private final ClienteRepository clienteRepository;
+
+    private final UsuarioService usuarioService;
+
+
+    public Cliente toCliente(ClienteCreateDTO createDTO){
+        Cliente cliente = new Cliente();
+        BeanUtils.copyProperties(createDTO,cliente);
+        cliente.setUsuario(usuarioService.buscarPorId(createDTO.getUsuarioId()));
+        return cliente;
+    }
+
+    public ClienteResponseDTO toDto(Cliente cliente){
+        ClienteResponseDTO responseDTO = new ClienteResponseDTO();
+        BeanUtils.copyProperties(cliente,responseDTO);
+        responseDTO.setUsuarioId(cliente.getUsuario().getId());
+        return responseDTO;
+    }
+
+    public List<ClienteResponseDTO> toListDto(List<Cliente> clienteList){
+        return clienteList.stream().map(responseDto -> toDto(responseDto)).collect(Collectors.toList());
+    }
 }
