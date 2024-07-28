12bb9486a33463623c1a1672594d7db0a1cc5154
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-28 16:10:28 -0300

    adicionando os metodos salvar e buscarPorId na classe ContaDigitalService

--- src/main/java/com/BankingAPI/service/ContaDigitalService.java
+++ src/main/java/com/BankingAPI/service/ContaDigitalService.java
@@ -2,11 +2,15 @@ package com.BankingAPI.service;
 
 import com.BankingAPI.dto.ContaDigitalCreateDTO;
 import com.BankingAPI.dto.ContaDigitalResponseDTO;
+import com.BankingAPI.exceptions.EntityNotFoundException;
+import com.BankingAPI.exceptions.UsernameUniqueViolationException;
 import com.BankingAPI.models.ContaDigital;
 import com.BankingAPI.repositories.ContaDigitalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +26,20 @@ public class ContaDigitalService {
     private final ClienteService clienteService;
 
 
+    @Transactional
+    public ContaDigital salvar(ContaDigital contaDigital){
+        try {
+            return contaDigitalRepository.save(contaDigital);
+        }catch (DataIntegrityViolationException e){
+            throw new UsernameUniqueViolationException(String.format("Conta com NUMERO: {%s} já cadastrado.", contaDigital.getNumero()));
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public ContaDigital buscarPorId(Long id){
+        return contaDigitalRepository.findById(id).orElseThrow(()->
+                new EntityNotFoundException(String.format("Id {%s} não encontrado !", id)));
+    }
     public ContaDigital toContaDigital(ContaDigitalCreateDTO createDTO){
         ContaDigital contaDigital = new ContaDigital();
         BeanUtils.copyProperties(createDTO,contaDigital);
