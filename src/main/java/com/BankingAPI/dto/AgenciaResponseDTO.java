88f8b9e0d4b98b7bbd0deeed792bd39e56ded428
Augusto LBA <j.augusto.lba@gmail.com>
2024-07-27 23:44:44 -0300

    adiconando a classe AgenciaResponseDTO

---
+++ src/main/java/com/BankingAPI/dto/AgenciaResponseDTO.java
@@ -0,0 +1,19 @@
+package com.BankingAPI.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AgenciaResponseDTO {
+
+    private String nome;
+
+    private int numero;
+    
+    private String telefone;
+}
